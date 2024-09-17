4e60e7b986a0a82d486ec2c08bb924e8e99e07e4
degviela <144688261+degviela@users.noreply.github.com>
2024-09-17 09:21:33 +0300

    Update Header.jsx
    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,11 +44,11 @@ const Header = () => {
                         placeholder="Search styles..."
                         value={searchQuery}
                         onChange={handleSearchChange}
-                        className="max-w-[80%] sm:w-[70%] p-2 rounded-3xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-500"
+                        className=" bg-transparent text-white max-w-[80%] sm:w-[70%] p-2 rounded-3xl border-2 border-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 placeholder-orange-500"
                     />
                     <button
                         type="submit"
-                        className="ml-2 p-2 bg-orange-500 text-white rounded-3xl hover:bg-orange-600 transition duration-300"
+                        className=" cursor-pointer ml-2 p-2 bg-orange-500 text-white rounded-3xl hover:bg-orange-600 transition duration-300"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
